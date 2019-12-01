59a603376b7bff7f5b24404f537e3c31307502d8
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-12-01 18:02:21 +0900

    fix: Linkコンポーネント使用に伴うテストの修正

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
