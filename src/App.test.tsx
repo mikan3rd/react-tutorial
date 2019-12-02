2793aa973cd2aca2d663011ebf72431a2d0d4bc5
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-12-03 00:04:02 +0900

    fix: testのimport修正

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
+import { App } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
