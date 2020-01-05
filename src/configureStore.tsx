593c2428ba5ae381d24de584845b0a995bc8a697
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-01-05 18:01:53 +0900

    chore: 記述箇所の修正

--- src/configureStore.tsx
+++ src/configureStore.tsx
@@ -9,30 +9,28 @@ import { State, rootReducer } from 'reducers';
 import { rootSaga } from 'sagas';
 import { JSObject } from 'types/Common';
 
+const logger = createLogger({
+  diff: true,
+  duration: true,
+  stateTransformer: (state: State) => {
+    const newState: JSObject = {};
+    for (const key in state) {
+      const targetState = state[key as keyof State];
+      if (isImmutable(targetState)) {
+        newState[key] = targetState.toJS();
+      } else {
+        newState[key] = targetState;
+      }
+    }
+    return newState;
+  },
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
 export const history = createBrowserHistory();
 
 export function configureStore(preloadedState?: State) {
-  const sagaMiddleware = createSagaMiddleware({
-    onError(error) {
-      throw error;
-    },
-  });
-  const logger = createLogger({
-    diff: true,
-    duration: true,
-    stateTransformer: (state: State) => {
-      const newState: JSObject = {};
-      for (const key in state) {
-        const targetState = state[key as keyof State];
-        if (isImmutable(targetState)) {
-          newState[key] = targetState.toJS();
-        } else {
-          newState[key] = targetState;
-        }
-      }
-      return newState;
-    },
-  });
   const middlewares = [routerMiddleware(history), sagaMiddleware, logger];
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const store = createStore(rootReducer(history), preloadedState, middlewareEnhancer);
